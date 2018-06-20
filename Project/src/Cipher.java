941379c3f950eeb2f36101ffa176d10bd1434035
xiwbo <bowwikatigbak16@gmail.com>
2018-06-20 17:06:29 +0800

    updated commit, new func, database added, still no login. yet.

--- Project/src/Cipher.java
+++ Project/src/Cipher.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
 public class Cipher extends HttpServlet
 {
 	private static String cryptoText = "";
 	private static int textShift = 0;
+	private static Connection v = null;
+	private static Statement stmt = null;
+	private static ResultSet res = null;
 
 	private static String encryptText(String text, int shift) {
 		String encrypted = "";
@@ -18,8 +22,48 @@ public class Cipher extends HttpServlet
 		return(encrypted);
 	}
 
+	public static Connection connectdb() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			v = DriverManager.getConnection("jdbc:mysql://localhost/db_projectciph?user=root&" +
+			"password=&serverTimezone=UTC&useSSL=false");
+		}
+		catch(Exception e) {
+			v = null;
+			e.printStackTrace();
+		}
+		finally {
+			try { if (stmt != null) stmt.close(); } catch (Exception e) {};
+		}
+		return(v);
+	}
+
 	public void init() throws ServletException {
 		encryptText(cryptoText, textShift);
+		Connection c = connectdb();
+		if(c == null) {
+			System.out.println("Error connecting to your MySQL Database...");
+		}
+		else {
+			System.out.println("Connected.");
+		}
+	}
+
+	public static void readFromMySQLDB(Connection conn) {
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement("INSERT INTO tbl_cipher(encryptedTxt, shiftNo) VALUES (?,?);");
+			stmt.setString(1, cryptoText);
+			stmt.setInt(2, textShift);
+			stmt.executeUpdate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try { if (res != null) res.close(); } catch (Exception e) {};
+			try { if (stmt != null) stmt.close(); } catch (Exception e) {};
+		}
 	}
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -29,31 +73,30 @@ public class Cipher extends HttpServlet
 		out.println("<div class='cell' id='cell1'>");
 		out.println("<div>");
 				out.println("<form action='./encrypt' method='GET'>");
-					out.println("<h3>Text to encrypt:</h3>");
+					out.println("<h3>Text to Encrypt/Decrypt:</h3>");
 					out.println("<input required type='text' name='encryptedText' id='textInput'>");
-					out.println("<h3>Number of shifts:</h3>");
+					out.println("<h3>Number of Rotation:</h3>");
 					out.println("<input min='-255' max='255' step='1' required type='number' id='num_id' name='shiftNumber'/><br><br>");
 					out.println("<input type='submit' value='Submit' id='submit'>");
+					out.println("<a href='/Project/data'><p>Click to see all encrypted data</p></a>");
 				out.println("</form>");
 			out.println("</div>");
 		out.println("</div>");
-		
 		cryptoText = req.getParameter("encryptedText").replaceAll("[ -+.^:,]", "");
 		String aString = req.getParameter("shiftNumber");
 		textShift = Integer.parseInt(aString);
+		readFromMySQLDB(v);
 		try {
 			if(cryptoText.matches("[a-zA-Z]+")) {
 				int shift = textShift;
 				out.println("<div class='cell' id='cell2'>");
 					out.println("<div>");
-						out.println("<h1>Text : </h1>" + "<h3>"+ req.getParameter("encryptedText").replaceAll("[-+.^:,]", "") + "</h3>");
-						out.println("Shift : " + shift);
-						out.println("Cipher: " + encryptText(cryptoText, shift));
+						out.println("<h1>Cipher: </h1>" + "<h2>" + encryptText(cryptoText, shift) + "</h2>");
 					out.println("</div>");
 				out.println("</div>");
 			}
 			else {
-				out.println("<div class='cell' id='cell2'><div>Text must be letters only.</div></div>");
+				out.println("<div class='cell' id='cell2'><div><h1>Text must be letters only.<h1></div></div>");
 				return;
 			}
 		}
@@ -65,5 +108,8 @@ public class Cipher extends HttpServlet
 	public void destroy() {
 		cryptoText = null;
 		textShift = 0;
+		v = null;
+		stmt = null;
+		res = null;
 	}
 }
---
+++ Project/src/Data.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class Data extends HttpServlet
+{
+	private static Statement stmt = null;
+	private static Connection conn = null;
+	private static ResultSet aResultSet = null;
+
+	public static Connection connectdb() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/db_projectciph?user=root&" +
+			"password=&serverTimezone=UTC&useSSL=false");
+		}
+		catch(Exception e) {
+			conn = null;
+			e.printStackTrace();
+		}
+		finally {
+			try { if (stmt != null) stmt.close(); } catch (Exception e) {};
+		}
+		return(conn);
+	}
+
+	public void init() throws ServletException {
+		Connection c = connectdb();
+		if(c == null) {
+			System.out.println("Error connecting to your MySQL Database...");
+		}
+		else {
+			System.out.println("Connected.");
+		}
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/xml");
+		PrintWriter out = res.getWriter();
+		out.println("<encrypt>");
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+			ResultSet aResultSet = stmt.executeQuery("SELECT * FROM tbl_cipher;");
+			while(aResultSet.next()) {
+				String[] str = {
+					aResultSet.getString("id"),
+					aResultSet.getString("encryptedTxt"),
+					aResultSet.getString("shiftNo")
+				};
+				for (int i = 0; i < str.length-2; i++) {
+					out.print("<data id= '" + str[0] + "' encryptedTxt= '" + str[1] + "' shiftNo= '" + str[2] + "'></data>" + "\n");
+				}
+			}
+			out.println("</encrypt>");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try { if (stmt != null) stmt.close(); } catch (Exception e) {};
+			try { if (aResultSet != null) aResultSet.close(); } catch (Exception e) {};
+		}
+	}
+
+	public void destroy() {
+		stmt = null;
+		conn = null;
+		aResultSet = null;
+	}
+}
